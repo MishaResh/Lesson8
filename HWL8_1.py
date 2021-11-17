02ac0db33c9a5218ed908cdacfd75c54b731290b
MishaResh <65731478+MishaResh@users.noreply.github.com>
2021-11-17 06:15:04 +0400

    Add files via upload

--- HWL8_1.py
+++ HWL8_1.py
@@ -3,24 +3,51 @@
 # извлекать число, месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod,
 # должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной
 # структуры на реальных данных.
+class InError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        return f'Ошибка в переданном аргументе {self.message}'
 
 
-def p_data(my_data: str):
-    print(f'p_data = {my_data}')
-    return True if my_data[:2].isdigit() else print('фигня')
-
-
-class Дата:
+class M_Data:
     my_data = str
+
+    def __init__(self, my_data):
+        self.my_data = my_data
+
     @staticmethod
-    def my_str(my_data: str):
-        dd = int(my_data[:2])
-        mm = int(my_data[my_data.find('-')+1:my_data.find('-',)+3])
-        yy = int(my_data[len(my_data)-4:len(my_data)])
-        return f'День -{dd} тип {type(dd)} Месяц - {mm} тип {type(mm)} Год - {yy} тип {type(yy)}'
+    def val_data(my_data: str):
+        my_data_l = []
+        if my_data[:2].isdigit() and 1 <= int(my_data[:2]) <= 31:
+            my_data_l.append(my_data[:2])
+        else:
+            raise InError(my_data[:2])
+        if my_data[my_data.find('-') + 1:my_data.find('-') + 3].isdigit() and 1 <= int(
+                my_data[my_data.find('-') + 1:my_data.find('-') + 3]) <= 12:
+            my_data_l.append(my_data[my_data.find('-') + 1:my_data.find('-') + 3])
+        else:
+            raise InError(my_data[my_data.find('-') + 1:my_data.find('-') + 3])
+        if my_data[len(my_data) - 4:len(my_data)].isdigit():
+            my_data_l.append(my_data[len(my_data) - 4:len(my_data)])
+        else:
+            raise InError(my_data[len(my_data) - 4:len(my_data)])
+
+        return my_data_l
+
     @classmethod
-    def isvlec(cls, my_data: str):
-        return True if my_data[:2].isdigit() else print('фигня')
+    def my_str(cls, my_data: str):
+        try:
+            dd = int(cls.val_data(my_data)[0])
+            mm = int(cls.val_data(my_data)[1])
+            yy = int(cls.val_data(my_data)[2])
+        except ValueError:
+            print('Ошибка! Не число')
+        return f'Число - {dd} его тип {type(dd)}, Месяц - {mm} его тип {type(mm)} Число - {yy} его тип {type(yy)}'
 
 
-print(Дата.my_str('22-10-2021'))
+print(M_Data.my_str('44-12-2021'))
