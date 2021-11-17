2238b4d3602929c45611e15238833640e966b637
MishaResh <65731478+MishaResh@users.noreply.github.com>
2021-11-17 19:20:29 +0400

    Add files via upload

--- HWL8_1.py
+++ HWL8_1.py
@@ -3,18 +3,8 @@
 # извлекать число, месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod,
 # должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной
 # структуры на реальных данных.
-class InError(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
 
-    def __str__(self):
-        return f'Ошибка в переданном аргументе {self.message}'
-
-
-class M_Data:
+class MData:
     my_data = str
 
     def __init__(self, my_data):
@@ -23,31 +13,37 @@ class M_Data:
     @staticmethod
     def val_data(my_data: str):
         my_data_l = []
+
         if my_data[:2].isdigit() and 1 <= int(my_data[:2]) <= 31:
             my_data_l.append(my_data[:2])
         else:
-            raise InError(my_data[:2])
+            my_data_l.append('**')
         if my_data[my_data.find('-') + 1:my_data.find('-') + 3].isdigit() and 1 <= int(
                 my_data[my_data.find('-') + 1:my_data.find('-') + 3]) <= 12:
             my_data_l.append(my_data[my_data.find('-') + 1:my_data.find('-') + 3])
         else:
-            raise InError(my_data[my_data.find('-') + 1:my_data.find('-') + 3])
+            my_data_l.append('**')
         if my_data[len(my_data) - 4:len(my_data)].isdigit():
             my_data_l.append(my_data[len(my_data) - 4:len(my_data)])
         else:
-            raise InError(my_data[len(my_data) - 4:len(my_data)])
+            my_data_l.append('**')
 
         return my_data_l
 
     @classmethod
     def my_str(cls, my_data: str):
+        dd, mm, yy = int, int, int
         try:
             dd = int(cls.val_data(my_data)[0])
             mm = int(cls.val_data(my_data)[1])
             yy = int(cls.val_data(my_data)[2])
         except ValueError:
-            print('Ошибка! Не число')
-        return f'Число - {dd} его тип {type(dd)}, Месяц - {mm} его тип {type(mm)} Число - {yy} его тип {type(yy)}'
+            print('Ошибка! Не число, или ошибка в дате')
+            exit()
+        else:
+            return f'Число - {dd} его тип {type(dd)}, Месяц - {mm} его тип {type(mm)} Число - {yy} его тип {type(yy)}'
 
 
-print(M_Data.my_str('44-12-2021'))
+print(MData.my_str('31-12-2021'))
+print(MData.my_str('44-12-2021'))
+print(MData.my_str('ии-12-2021'))
