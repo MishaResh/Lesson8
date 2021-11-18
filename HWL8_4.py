741a4bedd68d1a93bdd83b3ee55018c4378ca3d4
MishaResh <65731478+MishaResh@users.noreply.github.com>
2021-11-18 18:53:54 +0400

    Add files via upload

--- HWL8_4.py
+++ HWL8_4.py
@@ -2,6 +2,10 @@
 # «Оргтехника», который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники
 # (принтер, сканер, ксерокс). В базовом классе определить параметры, общие для приведенных типов.
 # В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+# 5. Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники на склад
+# и передачу в определенное подразделение компании. Для хранения данных о наименовании и количестве единиц
+# оргтехники, а также других данных, можно использовать любую подходящую структуру, например словарь.
+
 class Office_eq:
     type_eq: str  # тип оргтехники
     brand_eq: str  # производитель оргтехники (IBM)
@@ -13,7 +17,7 @@ class Office_eq:
         self.price_eq = price_eq
 
 
-class Stock_eq:
+class StockEq:
     stock_ed: int  # идентификатор склада
     max_count: int  # максимальное количество единиц оргтехники
     st_equipment: dict  # количество по типам
@@ -22,24 +26,43 @@ class Stock_eq:
         self.st_equipment = st_equipment
         self.max_count = max_count
         self.stock_ed = stock_ed
+
     @staticmethod
-    def entry_equipment(s_type:str, q_ty:int, st_eq: {}):
-        for key,value in st_eq.items():
-            if key == s_type:
-                value+=q_ty
+    def entry_equipment(s_type: str, q_ty: int, st_eq: dict):
+        if len(st_eq) == 0:
+            st_eq.update({s_type: q_ty})
+        elif s_type in st_eq:
+            st_eq[s_type] = st_eq.get(s_type) + q_ty
+        else:
+            st_eq.update({s_type: q_ty})
+
+    @staticmethod
+    def out_equipment(s_name: str, s_type: str, q_ty: int, st_eq: dict):
+        if len(st_eq) == 0:
+            print("Этот склад пуст")
+        elif s_type in st_eq:
+            if st_eq.get(s_type) >= q_ty:
+                st_eq[s_type] = st_eq.get(s_type) - q_ty
+                print(f'Отдаем в {s_name} все {q_ty} шт. {s_type}. Еще есть {st_eq.get(s_type)} шт.')
+            elif 0 < st_eq.get(s_type) < q_ty:
+                print(f'Отдаем в {s_name} все {st_eq.get(s_type)} шт. {s_type}. Больше {s_type} нет.')
+                st_eq[s_type] = 0
             else:
-                st_eq.update({s_type:q_ty})
+                print(f'Оборудования {s_type} на складе нет. Ждем поступлений')
+        else:
+            print(f'Оборудования {s_type} на складе нет. Ждем поступлений')
 
-
-
-    def stock_entry(self, s_type, quantity):
-        if (self.max_count - quantity) >= quantity:
+    def stock_entry(self, s_type, quantity: int):
+        if self.max_count >= quantity:
             self.max_count -= quantity
-            self.entry_equipment(s_type,quantity,self.st_equipment)
+            self.entry_equipment(s_type, quantity, self.st_equipment)
             print(f'Забираем все {quantity} {s_type}. Еще можем взять {self.max_count} единиц')
         else:
-            self.max_count -= self.max_count
             print(f'Берём {self.max_count} {s_type}. Больше не можем')
+
+    def stock_out(self, s_type, quantity: int, struct_name: str):
+        self.out_equipment(struct_name, s_type, quantity, self.st_equipment)
+
     @property
     def in_stock(self):
         pass
@@ -72,6 +95,12 @@ class copy_tech(Office_eq):
 printer_1 = m_printer('laser', 'printer', 'HP', 100)
 scaner_1 = m_Scan('A4', 'scaner', 'Sony', 150)
 copyr_1 = copy_tech('A4', 'copyr', 'Xerox', 80)
-stock_1 = Stock_eq(1, 50, {})
-stock_1.stock_entry('printer',25)
+stock_1 = StockEq(1, 50, {})
+stock_1.stock_entry('printer', 25)
+stock_1.stock_entry('scaner', 15)
+print(stock_1.st_equipment)
+stock_1.stock_out('scaner', 12, 'бухгалтерия')
+print(stock_1.st_equipment)
+stock_1.stock_entry('scaner', 1)
+stock_1.stock_out('scaner', 5, 'бухгалтерия')
 print(stock_1.st_equipment)
